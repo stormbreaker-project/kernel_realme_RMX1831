27265f498d0ee6e7c75497c7edc946d11e76ef23
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2020-08-01 12:29:51 +0530

    changes for rmx1831

--- clang_compile.sh
+++ clang_compile.sh
@@ -3,7 +3,7 @@ echo "Cloning dependencies"
 git clone --depth=1 https://github.com/crdroidmod/android_prebuilts_clang_host_linux-x86_clang-5407736 clang
 git clone --depth=1 https://github.com/KudProject/arm-linux-androideabi-4.9 gcc32
 git clone --depth=1 https://github.com/KudProject/aarch64-linux-android-4.9 gcc
-git clone https://gitlab.com/Baibhab34/AnyKernel3.git -b rm1 --depth=1 AnyKernel
+git clone https://github.com/SparXFusion/AnyKernel3.git -b cartel --depth=1 AnyKernel
 echo "Done"
 KERNEL_DIR=$(pwd)
 IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz-dtb"
@@ -12,12 +12,12 @@ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
 export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
 export ARCH=arm64
-export KBUILD_BUILD_USER=baibhab
-export KBUILD_BUILD_HOST=azure
+export KBUILD_BUILD_USER=sparxfusion
+export KBUILD_BUILD_HOST=chemsburger
 
 # Compile plox
 function compile() {
-    make -j$(nproc) O=out ARCH=arm64 oppo6771_17065_defconfig
+    make -j$(nproc) O=out ARCH=arm64 oppo6771_18611_defconfig
     make -j$(nproc) O=out \
                     ARCH=arm64 \
                     CC=clang \
@@ -30,8 +30,8 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Stormbreaker-RMX1801-HMP-${TANGGAL}.zip *
-    curl https://bashupload.com/Stormbreaker-RMX1801-HMP-${TANGGAL}.zip --data-binary @Stormbreaker-RMX1801-HMP-${TANGGAL}.zip
+    zip -r9 Stormbreaker-RMX1831-ALPHA${TANGGAL}.zip *
+    cd ..
 }
 compile
 zipping
